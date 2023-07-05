cc4e2790264be4ba33d6ad29742a6fce96aa3df9
Aeternus185 <andryp_@hotmail.com>
2023-07-05 17:02:57 -0400

    feat: 🔧 Add Montserrat and Lato to font families in Tailwind config

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,18 @@ import { SessionProvider } from "next-auth/react";
 import { type AppType } from "next/app";
 import { api } from "~/utils/api";
 import "~/styles/globals.css";
+import { Montserrat, Lato } from "next/font/google";
+
+const montserrat = Montserrat({
+  subsets: ["latin"],
+  variable: "--font-montserrat",
+});
+const lato = Lato({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+  style: ["normal", "italic"],
+  variable: "--font-lato",
+});
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
@@ -10,6 +22,14 @@ const MyApp: AppType<{ session: Session | null }> = ({
 }) => {
   return (
     <SessionProvider session={session}>
+      <style jsx global>
+        {`
+          :root {
+            --font-montserrat: ${montserrat.style.fontFamily};
+            --font-lato: ${lato.style.fontFamily};
+          }
+        `}
+      </style>
       <Component {...pageProps} />
     </SessionProvider>
   );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,8 +1,13 @@
 import { type Config } from "tailwindcss";
+import { fontFamily } from "tailwindcss/defaultTheme";
 
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    fontFamily: {
+      montserrat: ["var(--font-montserrat)", ...fontFamily.sans],
+      lato: ["var(--font-lato)", ...fontFamily.sans],
+    },
     extend: {},
   },
   plugins: [],
