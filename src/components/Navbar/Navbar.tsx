2990c6b2f14fbc6f22dd93f59f10115ff8c19fd9
Aeternus185 <andryp_@hotmail.com>
2023-07-12 00:48:41 -0400

    feat: 📱 Add responsive for lg screens

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,12 +1,13 @@
-import { useEffect } from "react";
-import { useState } from "react";
-import ThemeToggle from "../ThemeToggle/ThemeToggle";
-import LangDropdown from "../LangDropdown/LangDropdown";
+import { useEffect, useState } from "react";
 import Button from "../Button/Button";
 import { List, X } from "@phosphor-icons/react";
+import { useTranslation } from "next-i18next";
+import { useRouter } from "next/router";
 
 function Navbar() {
+  const { t } = useTranslation("common");
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const router = useRouter();
 
   const handleMenuClick = () => {
     setIsMenuOpen(!isMenuOpen);
@@ -25,34 +26,81 @@ function Navbar() {
   }, [isMenuOpen]);
 
   return (
-    <nav>
-      <div className="flex items-center justify-between px-5 py-4">
-        <div className="font-montserrat text-xl font-bold">Giftstorming Ai</div>
-        <div className="flex items-center gap-2">
-          <LangDropdown />
-          <ThemeToggle />
-          <button
-            onClick={handleMenuClick}
-            className="flex items-center justify-center"
-          >
-            {isMenuOpen ? (
-              <X size={24} className="text-neutral-800 dark:text-neutral-200" />
-            ) : (
-              <List
-                size={24}
-                className="text-neutral-800 dark:text-neutral-200"
-              />
-            )}
-          </button>
+    <nav className="sticky top-0 z-50">
+      <div
+        className={`flex items-center justify-between px-5 py-4 lg:px-24 lg:py-8 ${
+          isMenuOpen
+            ? "bg-neutral-50 dark:bg-neutral-950"
+            : "bg-neutral-50 bg-opacity-50 backdrop-blur-sm dark:bg-neutral-950 dark:bg-opacity-50"
+        }`}
+      >
+        <div className="font-montserrat text-xl font-bold lg:text-2xl">
+          Giftstorming Ai
         </div>
+        <div className="hidden lg:flex lg:items-center lg:justify-between">
+          <div className="flex justify-center gap-8">
+            <a
+              href="#"
+              className={router.pathname === "/" ? "text-red-500" : ""}
+            >
+              {t("home")}
+            </a>
+            <a
+              href="#how-it-works"
+              className={
+                router.pathname === "/how-it-works" ? "text-red-500" : ""
+              }
+            >
+              {t("how_it_works")}
+            </a>
+            <a
+              href="#features"
+              className={router.pathname === "/features" ? "text-red-500" : ""}
+            >
+              {t("features")}
+            </a>
+          </div>
+        </div>
+        <div className="hidden lg:block">
+          <Button variant="contained" size="md">
+            {t("get_early_access")}
+          </Button>
+        </div>
+        <button
+          onClick={handleMenuClick}
+          className="flex items-center justify-center lg:hidden"
+        >
+          {isMenuOpen ? (
+            <X size={24} className="text-neutral-800 dark:text-neutral-200" />
+          ) : (
+            <List
+              size={24}
+              className="text-neutral-800 dark:text-neutral-200"
+            />
+          )}
+        </button>
       </div>
       {isMenuOpen && (
-        <div className="fixed left-0 top-14 z-10 flex h-full w-full flex-col items-center justify-start gap-6 bg-neutral-50 px-5 py-4 font-lato dark:bg-neutral-950">
-          <a href="#">Home</a>
-          <a href="#how-it-works">How it works</a>
-          <a href="#features">Features</a>
+        <div className="fixed left-0 top-14 z-10 flex h-full w-full flex-col items-center justify-start gap-6 bg-neutral-50 px-5 py-4 font-lato dark:bg-neutral-950 lg:hidden">
+          <a href="#" className={router.pathname === "/" ? "text-red-500" : ""}>
+            {t("home")}
+          </a>
+          <a
+            href="#how-it-works"
+            className={
+              router.pathname === "/how-it-works" ? "text-red-500" : ""
+            }
+          >
+            {t("how_it_works")}
+          </a>
+          <a
+            href="#features"
+            className={router.pathname === "/features" ? "text-red-500" : ""}
+          >
+            {t("features")}
+          </a>
           <Button variant="contained" size="lg" fullWidth>
-            Get Early Access
+            {t("get_early_access")}
           </Button>
         </div>
       )}
