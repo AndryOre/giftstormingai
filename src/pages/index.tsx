c53f7574b40487dd71e7c142b4997172acc63c64
Aeternus185 <andryp_@hotmail.com>
2023-07-12 16:48:56 -0400

    feat: ✨ Add how it works section, features section and cta

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,6 +14,7 @@ import {
   Sun,
   Heart,
 } from "@phosphor-icons/react";
+import SectionTitle from "~/components/SectionTitle/SectionTitle";
 import StepCard from "~/components/Cards/StepCard/StepCard";
 import FeatureCard from "~/components/Cards/FeatureCard/FeatureCard";
 import Footer from "~/components/Footer/Footer";
@@ -34,7 +35,7 @@ const Home: NextPage = () => {
         <section className="flex flex-col items-center justify-center gap-8 px-6 py-10 text-center lg:flex-row lg:items-start lg:justify-between lg:px-24 lg:text-left">
           <div className="flex flex-col gap-6 lg:mt-8 lg:gap-10">
             <div className="flex flex-col gap-4 lg:text-lg">
-              <h1 className="font-montserrat text-3xl font-bold lg:text-5xl">
+              <div className="font-montserrat text-3xl font-bold lg:text-5xl">
                 <span className="bg-gradient-to-t from-neutral-950 to-neutral-950/50 bg-clip-text text-transparent dark:bg-gradient-to-b dark:from-neutral-50 dark:to-neutral-50/50">
                   {t("discover_perfect_gift_start")}
                 </span>
@@ -47,10 +48,10 @@ const Home: NextPage = () => {
                 <span className="bg-gradient-to-t from-red-500 to-red-500/50 bg-clip-text text-transparent dark:bg-gradient-to-b">
                   {t("ai")}
                 </span>
-              </h1>
-              <p className="font-lato text-neutral-800 dark:text-neutral-200">
+              </div>
+              <div className="font-lato text-neutral-800 dark:text-neutral-200">
                 {t("storm_of_gift_ideas")}
-              </p>
+              </div>
             </div>
             <div className="flex flex-col items-center gap-4 lg:flex-row">
               <div className="w-full lg:w-auto">
@@ -76,6 +77,77 @@ const Home: NextPage = () => {
               />
               Your browser does not support the video tag.
             </video>
+          </div>
+        </section>
+        <section className="flex flex-col items-center justify-center gap-8 px-6 py-10 text-center lg:px-24">
+          <SectionTitle title={t("how_it_works")} />
+          <div className="flex w-full flex-col justify-center bg-gradient-to-t from-neutral-950 to-neutral-950/50 bg-clip-text text-center font-montserrat text-2xl font-bold text-transparent dark:bg-gradient-to-b dark:from-neutral-50 dark:to-neutral-50/50 lg:text-3xl">
+            {t("follow_simple_steps")}
+          </div>
+          <div className="flex flex-col gap-6 text-left lg:flex-row">
+            <StepCard
+              step={1}
+              title={t("tell_us_about_gift")}
+              description={t("who_is_lucky_one")}
+            />
+            <StepCard
+              step={2}
+              title={t("hit_magic_button")}
+              description={t("done_with_details")}
+            />
+            <StepCard
+              step={3}
+              title={t("get_your_personalized_gift_ideas")}
+              description={t("voila_gift_ideas")}
+            />
+          </div>
+        </section>
+        <section className="flex flex-col items-center justify-center gap-8 px-6 py-10 text-center lg:px-24">
+          <SectionTitle title={t("features")} />
+          <div className="flex w-full flex-col justify-center bg-gradient-to-t from-neutral-950 to-neutral-950/50 bg-clip-text text-center font-montserrat text-2xl font-bold text-transparent dark:bg-gradient-to-b dark:from-neutral-50 dark:to-neutral-50/50 lg:text-3xl">
+            {t("explore_features")}
+          </div>
+          <div className="flex flex-col gap-6 text-left lg:flex-row">
+            <FeatureCard
+              icon={<CursorClick />}
+              title={t("easy_peasy")}
+              description={t("user_friendly_interface")}
+              color="yellow"
+            />
+            <FeatureCard
+              icon={<Gift />}
+              title={t("tailored_just_for_you")}
+              description={t("personalized_gift_ideas")}
+              color="green"
+            />
+            <FeatureCard
+              icon={<Translate />}
+              title={t("lost_in_translation")}
+              description={t("comfortable_in_english_or_spanish")}
+              color="blue"
+            />
+            <FeatureCard
+              icon={<Sun />}
+              title={t("day_or_night_we_got_the_look")}
+              description={t("choose_light_dark_theme")}
+              color="purple"
+            />
+            <FeatureCard
+              icon={<Heart />}
+              title={t("bookmark_your_faves")}
+              description={t("add_to_favorites")}
+              color="red"
+            />
+          </div>
+        </section>
+        <section className="flex flex-col items-center justify-center gap-8 px-6 py-10 text-center lg:px-24">
+          <div className="flex w-full flex-col items-center justify-center gap-4 rounded bg-gradient-to-bl from-red-500 to-red-600 p-6 shadow-[0px_2px_4px_-1px_rgba(239,68,68,0.06),0px_4px_6px_-1px_rgba(239,68,68,0.10)] lg:py-12">
+            <div className="font-montserrat text-2xl font-semibold text-neutral-50">
+              {t("ready_to_find_gift")}
+            </div>
+            <Button variant="contained" size="md">
+              {t("get_early_access")}
+            </Button>
           </div>
         </section>
         <Footer />
