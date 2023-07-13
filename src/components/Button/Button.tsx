e45b15a55f75b53c07ba18b751ed8cec843f406e
Aeternus185 <andryp_@hotmail.com>
2023-07-12 20:40:53 -0400

    feat: ✨ Enhance Button component with color primary and secondary

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -5,6 +5,7 @@ interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   onClick?: MouseEventHandler<HTMLButtonElement>;
   variant?: "text" | "outlined" | "contained";
   size?: "sm" | "md" | "lg";
+  color?: "primary" | "secondary";
   disabled?: boolean;
   startIcon?: ReactNode;
   endIcon?: ReactNode;
@@ -17,6 +18,7 @@ function Button({
   onClick,
   variant = "contained",
   size = "sm",
+  color = "primary",
   disabled = false,
   startIcon,
   endIcon,
@@ -45,18 +47,30 @@ function Button({
   switch (variant) {
     case "outlined":
       colorClasses = disabled
-        ? "border-2 text-red-300 border-red-300 cursor-not-allowed"
-        : "border-2 border-red-500 text-red-500 hover:text-neutral-50 focus:bg-red-700 focus:text-neutral-50 focus:ring-2 focus:ring-red-500 focus:ring-opacity-50 hover:bg-red-500 shadow dark:shadow-[0px_1px_3px_rgba(255,255,255,0.1),0px_1px_2px_-1px_rgba(255,255,255,0.1)]";
+        ? color === "primary"
+          ? "border-2 text-red-300 border-red-300 cursor-not-allowed"
+          : "border-2 text-neutral-300 border-neutral-300 cursor-not-allowed"
+        : color === "primary"
+        ? "border-2 border-red-500 text-red-500 hover:text-neutral-50 focus:bg-red-700 focus:ring-2 focus:ring-red-500 focus:ring-opacity-50 hover:bg-red-500 shadow dark:shadow-[0px_1px_3px_rgba(255,255,255,0.1),0px_1px_2px_-1px_rgba(255,255,255,0.1)]"
+        : "border-2 border-neutral-50 text-neutral-50 hover:text-red-500 focus:bg-neutral-200 focus:ring-2 focus:ring-neutral-50 focus:ring-opacity-50 hover:bg-neutral-50 shadow-md";
       break;
     case "text":
       colorClasses = disabled
-        ? "text-red-300 cursor-not-allowed"
-        : "text-red-500 hover:text-red-700 dark:hover:text-red-300";
+        ? color === "primary"
+          ? "text-red-300 cursor-not-allowed"
+          : "text-neutral-300 cursor-not-allowed"
+        : color === "primary"
+        ? "text-red-500 hover:text-red-700 dark:hover:text-red-300"
+        : "text-neutral-50 hover:text-neutral-200";
       break;
     default:
       colorClasses = disabled
-        ? "bg-red-200 text-red-300 cursor-not-allowed"
-        : "bg-red-500 text-neutral-50 hover:bg-red-700 focus:ring-2 focus:ring-red-500 focus:ring-opacity-50 shadow dark:shadow-[0px_1px_3px_rgba(255,255,255,0.1),0px_1px_2px_-1px_rgba(255,255,255,0.1)]";
+        ? color === "primary"
+          ? "bg-red-200 text-red-300 cursor-not-allowed"
+          : "bg-neutral-200 text-neutral-300 cursor-not-allowed"
+        : color === "primary"
+        ? "bg-red-500 text-neutral-50 hover:bg-red-700 focus:ring-2 focus:ring-red-500 focus:ring-opacity-50 shadow dark:shadow-[0px_1px_3px_rgba(255,255,255,0.1),0px_1px_2px_-1px_rgba(255,255,255,0.1)]"
+        : "bg-neutral-50 text-red-500 hover:bg-neutral-200 focus:ring-2 focus:ring-neutral-50 focus:ring-opacity-50 shadow-md";
   }
 
   return (
